ff1d68bf8c824323637b2778613e30b5b360efb3
Your NameBogdan <bogdantigheci@yahoo.com>
2020-03-20 12:11:03 +0200

    fixed new arrivals homepage position

--- client/src/components/utils/cardBlock.js
+++ client/src/components/utils/cardBlock.js
@@ -18,7 +18,13 @@ const CardBlock = props => {
     <div className="card_block">
       <div className="container">
         {props.title ? <div className="title">{props.title}</div> : null}
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center'
+          }}
+        >
           {renderCards(props.list)}
         </div>
       </div>
