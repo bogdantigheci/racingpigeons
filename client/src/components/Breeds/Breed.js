929d8392bb288cdf9895471d150b69b5b39eb837
Your NameBogdan <bogdantigheci@yahoo.com>
2020-03-25 12:14:12 +0200

    fixed breed css

--- client/src/components/Breeds/Breed.js
+++ client/src/components/Breeds/Breed.js
@@ -6,36 +6,41 @@ const Breed = props => {
     <div className="container d-flex flex-wrap">
       {props.breeds
         ? props.breeds.map((breed, i) => (
-            <div key={i}>
-              <div className="row">
-                <div className="col-md-6" style={{ width: '30rem' }}>
-                  <div className="card flex-md-row mb-4 shadow-sm h-md-250">
-                    <div className="card-body d-flex flex-column align-items-start ">
-                      <strong className="d-inline-block mb-2 text-primary">
-                        {breed.name}
-                      </strong>
-                      <p className="card-text mb-auto overflow-hidden breed_card_description">
-                        {breed.description}
-                      </p>
-                      <a
-                        className="btn btn-outline-primary btn-sm"
-                        role="button"
-                        href="/"
-                      >
-                        Continue reading
-                      </a>
-                    </div>
-                    <img
-                      className="card-img-right flex-auto d-none d-lg-block"
-                      src={_.get(
-                        breed,
-                        'images[0].url',
-                        `/images/image_not_available.png`
-                      )}
-                      alt={breed.name}
-                      style={{ width: '200px', height: '250px' }}
-                    />
-                  </div>
+            <div className="card promoting-card breeds_display">
+              <div className="card-body d-flex flex-row">
+                <div className="mx-auto">
+                  <h4 className="card-title font-weight-bold mb-2 breed_card_title">
+                    {breed.name}
+                  </h4>
+                </div>
+              </div>
+
+              <div className="view overlay">
+                <img
+                  className="card-img-top rounded-0 breed_image"
+                  src={_.get(
+                    breed,
+                    'images[0].url',
+                    `/images/image_not_available.png`
+                  )}
+                  alt="Card image cap"
+                />
+              </div>
+
+              <div className="card-body">
+                <div className="collapse-content">
+                  <p className="card-text collapse" id="collapseContent">
+                    {breed.description}
+                  </p>
+                  <a
+                    className="btn btn-flat red-text p-1 my-1 mr-0 mml-1 collapsed breed_read_more"
+                    data-toggle="collapse"
+                    href="#collapseContent"
+                    aria-expanded="false"
+                    aria-controls="collapseContent"
+                  >
+                    Read more
+                  </a>
                 </div>
               </div>
             </div>
