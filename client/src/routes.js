0b68a87395dae92016f5bcaa99b202fea4260749
Your NameBogdan <bogdantigheci@yahoo.com>
2020-04-21 16:27:46 +0300

    removed chat page

--- client/src/routes.js
+++ client/src/routes.js
@@ -22,7 +22,6 @@ import Forum from './components/Forum/Posts/Posts';
 import Post from './components/Forum/Comments/Post';
 import Club from './components/Club/Club';
 import BreederInfo from './components/Breeders/BreederInfo';
-import Chat from './components/Chat/Chat';
 import Weather from './components/Weather/Weather';
 
 const Routes = () => {
@@ -76,7 +75,6 @@ const Routes = () => {
 
         <Route path="/forum/post/:id" exact component={Auth(Post, true)} />
         <Route path="/forum" exact component={Auth(Forum, true)} />
-        <Route path="/chat" exact component={Auth(Chat, true)} />
         <Route path="/breeds" exact component={Auth(Breeds, null)} />
         <Route path="/weather" exact component={Auth(Weather, null)} />
         <Route path="/breeders" exact component={Auth(Breeders, null)} />
