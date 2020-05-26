eef724ebd1b28f91feef2ca8d155b9085f280e7d
Your NameBogdan <bogdantigheci@yahoo.com>
2020-05-26 09:45:20 +0300

    added flags to lang change buttons and focus to nav links

--- client/src/components/utils/changeLanguage.js
+++ client/src/components/utils/changeLanguage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
+import Flag from 'react-world-flags';
 
 const ChangeLanguage = ({ i18n }) => {
   const changeLanguage = (lng) => {
@@ -7,8 +8,12 @@ const ChangeLanguage = ({ i18n }) => {
   };
   return (
     <React.Fragment>
-      <button onClick={() => changeLanguage('ro')}>ro</button>
-      <button onClick={() => changeLanguage('en')}>en</button>
+      <button className="lang-button" onClick={() => changeLanguage('ro')}>
+        <Flag code="ro" height="16" />
+      </button>
+      <button className="lang-button" onClick={() => changeLanguage('en')}>
+        <Flag code="gb" height="16" />
+      </button>
     </React.Fragment>
   );
 };
